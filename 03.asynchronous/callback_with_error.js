de79ed7cd26a582836e64e76fc316607ce34c507
utsubo256 <96920121+utsubo256@users.noreply.github.com>
2026-02-04 17:55:39 +0900

    関数内でその関数のthisを使用していない関数をアロー関数に置き換えるよう修正

--- 03.asynchronous/callback_with_error.js
+++ 03.asynchronous/callback_with_error.js
@@ -3,17 +3,13 @@ import sqlite3 from "sqlite3";
 const db = new (sqlite3.verbose().Database)(":memory:");
 
 db.run("CREATE TABLE books (id INTEGER PRIMARY KEY ASC, title TEXT)", () => {
-  db.run(
-    "INSERT___INTO books (title) VALUES (?)",
-    "Rubyのしくみ",
-    function (err) {
+  db.run("INSERT___INTO books (title) VALUES (?)", "Rubyのしくみ", (err) => {
+    if (err) console.error(err);
+    db.get("SELECT___id, title FROM books", (err) => {
       if (err) console.error(err);
-      db.get("SELECT___id, title FROM books", (err) => {
-        if (err) console.error(err);
-        db.run("DROP TABLE books", () => {
-          db.close();
-        });
+      db.run("DROP TABLE books", () => {
+        db.close();
       });
-    },
-  );
+    });
+  });
 });
