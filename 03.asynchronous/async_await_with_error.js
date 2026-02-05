108fe22ab68b17be1b484050afa19ee1c2d8dc74
utsubo256 <96920121+utsubo256@users.noreply.github.com>
2026-02-05 16:01:21 +0900

    top-level awaitを使用するよう修正

--- 03.asynchronous/async_await_with_error.js
+++ 03.asynchronous/async_await_with_error.js
@@ -7,27 +7,23 @@ import {
 
 const db = new (sqlite3.verbose().Database)(":memory:");
 
-async function main() {
+await runPromise(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY ASC, title TEXT)",
+);
+try {
   await runPromise(
     db,
-    "CREATE TABLE books (id INTEGER PRIMARY KEY ASC, title TEXT)",
+    "INSERT___INTO books (title) VALUES (?)",
+    "Rubyのしくみ",
   );
-  try {
-    await runPromise(
-      db,
-      "INSERT___INTO books (title) VALUES (?)",
-      "Rubyのしくみ",
-    );
-  } catch (err) {
-    console.error(err.message);
-  }
-  try {
-    await getPromise(db, "SELECT___id, title FROM books");
-  } catch (err) {
-    console.error(err.message);
-  }
-  await runPromise(db, "DROP TABLE books");
-  await closePromise(db);
+} catch (err) {
+  console.error(err.message);
 }
-
-main();
+try {
+  await getPromise(db, "SELECT___id, title FROM books");
+} catch (err) {
+  console.error(err.message);
+}
+await runPromise(db, "DROP TABLE books");
+await closePromise(db);
--- 03.asynchronous/async_await_without_error.js
+++ 03.asynchronous/async_await_without_error.js
@@ -7,21 +7,17 @@ import {
 
 const db = new (sqlite3.verbose().Database)(":memory:");
 
-async function main() {
-  await runPromise(
-    db,
-    "CREATE TABLE books (id INTEGER PRIMARY KEY ASC, title TEXT)",
-  );
-  const insertedBook = await runPromise(
-    db,
-    "INSERT INTO books (title) VALUES (?)",
-    "Rubyのしくみ",
-  );
-  console.log(`id: ${insertedBook.lastID}`);
-  const book = await getPromise(db, "SELECT id, title FROM books");
-  console.log(`id: ${book.id}、title: ${book.title}`);
-  await runPromise(db, "DROP TABLE books");
-  await closePromise(db);
-}
-
-main();
+await runPromise(
+  db,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY ASC, title TEXT)",
+);
+const insertedBook = await runPromise(
+  db,
+  "INSERT INTO books (title) VALUES (?)",
+  "Rubyのしくみ",
+);
+console.log(`id: ${insertedBook.lastID}`);
+const book = await getPromise(db, "SELECT id, title FROM books");
+console.log(`id: ${book.id}、title: ${book.title}`);
+await runPromise(db, "DROP TABLE books");
+await closePromise(db);
